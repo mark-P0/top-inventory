aaa0db9f2900942396fd18ac3e4b5adaac3db1ac
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2025-08-16 00:33:26 +0800

    style(ui): format init files with biome

--- apps/ui/src/routes/__root.tsx
+++ apps/ui/src/routes/__root.tsx
@@ -1,25 +1,25 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanstackDevtools } from '@tanstack/react-devtools'
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { TanstackDevtools } from "@tanstack/react-devtools";
 
-import Header from '../components/Header'
+import Header from "../components/Header";
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <Header />
-      <Outlet />
-      <TanstackDevtools
-        config={{
-          position: 'bottom-left',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
-    </>
-  ),
-})
+	component: () => (
+		<>
+			<Header />
+			<Outlet />
+			<TanstackDevtools
+				config={{
+					position: "bottom-left",
+				}}
+				plugins={[
+					{
+						name: "Tanstack Router",
+						render: <TanStackRouterDevtoolsPanel />,
+					},
+				]}
+			/>
+		</>
+	),
+});
