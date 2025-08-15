58af2f580e3c08a1133fb1f86c9cfd67f2b7c851
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2025-08-16 01:19:09 +0800

    feat(ui.categories): init home page with sample data

--- apps/ui/src/routes/index.tsx
+++ apps/ui/src/routes/index.tsx
@@ -1,39 +1,61 @@
 import { createFileRoute } from "@tanstack/react-router";
-import logo from "../logo.svg";
+
+const SAMPLE_DATA = (() => {
+	type Category = {
+		id: string;
+		name: string;
+		slug: string;
+		item_type_ct: number;
+		total_item_ct: number;
+	};
+
+	function randomInteger() {
+		const array = crypto.getRandomValues(new Uint8Array(1));
+		return array[0];
+	}
+
+	const categories = Array.from({ length: 8 }, (_, idx) => {
+		const category: Category = {
+			id: crypto.randomUUID(),
+			name: `Category ${idx}`,
+			slug: `category-${idx}`,
+			item_type_ct: randomInteger(),
+			total_item_ct: randomInteger(),
+		};
+
+		return category;
+	});
+
+	return { categories };
+})();
 
 export const Route = createFileRoute("/")({
 	component: App,
 });
 
 function App() {
+	const { categories } = SAMPLE_DATA;
+
 	return (
-		<div className="text-center">
-			<header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-				<img
-					src={logo}
-					className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-					alt="logo"
-				/>
-				<p>
-					Edit <code>src/routes/index.tsx</code> and save to reload.
-				</p>
-				<a
-					className="text-[#61dafb] hover:underline"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<a
-					className="text-[#61dafb] hover:underline"
-					href="https://tanstack.com"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn TanStack
-				</a>
+		<main className="min-h-screen dark:text-stone-50 dark:bg-stone-700 p-6 space-y-6">
+			<header className="flex items-center justify-between gap-3">
+				<div>
+					<h1>Categories</h1>
+					<p>Inventory App</p>
+				</div>
 			</header>
-		</div>
+
+			<ol className="grid grid-cols-3 gap-3">
+				{categories.map((category) => (
+					<li key={category.id}>
+						<article className="dark:bg-stone-500">
+							<pre className="overflow-hidden">
+								{JSON.stringify(category, null, 2)}
+							</pre>
+						</article>
+					</li>
+				))}
+			</ol>
+		</main>
 	);
 }
