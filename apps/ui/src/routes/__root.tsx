d8fedf7c2614d33b6735ded294ba9e1116f44ce1
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2025-08-16 00:43:04 +0800

    feat(ui): remove init header component

--- apps/ui/src/routes/__root.tsx
+++ apps/ui/src/routes/__root.tsx
@@ -1,13 +1,10 @@
+import { TanstackDevtools } from "@tanstack/react-devtools";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import { TanstackDevtools } from "@tanstack/react-devtools";
-
-import Header from "../components/Header";
 
 export const Route = createRootRoute({
 	component: () => (
 		<>
-			<Header />
 			<Outlet />
 			<TanstackDevtools
 				config={{
