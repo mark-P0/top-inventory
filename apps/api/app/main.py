b4558821e990f605ea1328ef89a82fa0d4b7b6ed
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2025-08-16 16:07:39 +0800

    feat(api.openapi): simplify openapi operation ids

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
+from app.lib.fastapi_helpers import use_route_names_as_operation_ids
 from app.routes.categories import CategoriesRouter
 
 app = FastAPI()
 
 app.include_router(CategoriesRouter)
+
+
+use_route_names_as_operation_ids(app)
