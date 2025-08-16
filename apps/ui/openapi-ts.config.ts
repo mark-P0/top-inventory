6172c32f5d846f4dc365ab5a791326405ca866c3
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2025-08-16 15:47:44 +0800

    refactor(ui.env): get openapi path from dedicated env file

--- apps/ui/openapi-ts.config.ts
+++ apps/ui/openapi-ts.config.ts
@@ -1,8 +1,9 @@
+import { env } from './src/core/env';
 import { defineConfig } from '@hey-api/openapi-ts';
 
 export default defineConfig({
   input: {
-    path: 'http://localhost:8000/openapi.json',
+    path: env.OPENAPI_PATH,
     watch: true
   },
   output: './src/core/api/codegen',
---
+++ apps/ui/src/core/env.ts
@@ -0,0 +1,4 @@
+export const env = {
+	OPENAPI_PATH:
+		import.meta.env.OPENAPI_PATH ?? "http://localhost:8000/openapi.json",
+};
