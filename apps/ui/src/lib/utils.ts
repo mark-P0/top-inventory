ee452e009eec64392d31581de7157ebe32abc197
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2025-08-20 00:36:00 +0800

    fix(ui.lib.utils): types after cheatsheet addition

--- apps/ui/src/lib/utils.ts
+++ apps/ui/src/lib/utils.ts
@@ -1,20 +1,33 @@
 export function randomInteger() {
 	const array = crypto.getRandomValues(new Uint8Array(1));
 
-	return array[0];
+	const int = array[0];
+	if (int === undefined) {
+		throw new Error("No integer?");
+	}
+
+	return int;
 }
 
 /** Allows "overflow" and "underflow" indexing */
 export function wrappedAccess<T>(sequence: ArrayLike<T>, index: number): T {
 	const length = sequence.length;
 
+	if (length === 0) {
+		throw new Error("Empty sequence");
+	}
+
+	let item = sequence[index];
 	if (index < 0) {
-		return sequence[length + index];
+		item = sequence[length + index];
 	}
-
 	if (index >= length) {
-		return sequence[index - length];
+		item = sequence[index - length];
 	}
 
-	return sequence[index];
+	if (item === undefined) {
+		throw new Error("Failed to access item");
+	}
+
+	return item;
 }
