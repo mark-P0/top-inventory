9988202914894ae4b301c41262db6838253c2cf4
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2025-08-20 00:24:49 +0800

    refactor(api.routes.categories): use generator to simplify response data

--- apps/api/app/routes/categories/__init__.py
+++ apps/api/app/routes/categories/__init__.py
@@ -14,14 +14,16 @@ CategoriesRouter = APIRouter(prefix="/categories")
 
 @CategoriesRouter.get("/")
 def get_categories(query: GetCategoriesQuery) -> GetCategoriesResponse:
-    data = [
-        PublicCategory(
-            name_id=category.name_id,
-            name=category.name,
-            item_type_ct=randint(0, 32) if query.include_item_type_ct else None,
-            total_item_ct=randint(0, 32) if query.include_total_item_ct else None,
-        )
-        for category in sample_categories
-    ]
+    def generate_data_response():
+        for category in sample_categories:
+            item_type_ct = randint(0, 32) if query.include_item_type_ct else None
+            total_item_ct = randint(0, 32) if query.include_total_item_ct else None
 
-    return GetCategoriesResponse(data=data)
+            yield PublicCategory(
+                name_id=category.name_id,
+                name=category.name,
+                item_type_ct=item_type_ct,
+                total_item_ct=total_item_ct,
+            )
+
+    return GetCategoriesResponse(data=[*generate_data_response()])
