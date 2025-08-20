cff3cd3272813cb157862aeb6af00fb5a7676a5d
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2025-08-21 01:43:02 +0800

    feat(ui.routes.category): init cards

--- apps/ui/src/routes/$categoryNameId/index.tsx
+++ apps/ui/src/routes/$categoryNameId/index.tsx
@@ -1,6 +1,39 @@
-import { getCategories } from "@/core/api/codegen";
+import {
+	type PublicCategory,
+	type PublicCategoryItem,
+	type PublicCategoryItemType,
+	getCategories,
+} from "@/core/api/codegen";
+import { Card } from "@/core/components/Card";
 import { Screen } from "@/core/components/Screen";
+import { cn } from "@/lib/tailwind";
 import { createFileRoute, notFound } from "@tanstack/react-router";
+
+function groupItemTypeItems(category: PublicCategory) {
+	type GroupEntry = {
+		itemType: PublicCategoryItemType;
+		items: PublicCategoryItem[];
+	};
+
+	const entries: GroupEntry[] = [];
+	for (const item of category.items) {
+		let entry = entries.find(
+			({ itemType }) => itemType.uuid === item.item_type.uuid,
+		);
+		if (entry === undefined) {
+			entry = {
+				itemType: item.item_type,
+				items: [],
+			};
+
+			entries.push(entry);
+		}
+
+		entry.items.push(item);
+	}
+
+	return entries;
+}
 
 export const Route = createFileRoute("/$categoryNameId/")({
 	loader: async ({ params }) => {
@@ -26,7 +59,9 @@ export const Route = createFileRoute("/$categoryNameId/")({
 			throw notFound();
 		}
 
-		return { category };
+		const itemTypeItems = groupItemTypeItems(category);
+
+		return { category, itemTypeItems };
 	},
 	component: CategoryScreen,
 });
@@ -36,7 +71,49 @@ function CategoryScreen() {
 
 	return (
 		<Screen title={category.name}>
-			<pre>{JSON.stringify(category, null, 2)}</pre>
+			<ItemTypeList />
 		</Screen>
+	);
+}
+
+function ItemTypeList() {
+	const { itemTypeItems } = Route.useLoaderData();
+
+	return (
+		<ol className="grid lg:grid-cols-5 gap-3">
+			{itemTypeItems.map(({ itemType, items }) => (
+				<li key={itemType.uuid}>
+					<ItemTypeCard itemType={itemType} items={items} />
+				</li>
+			))}
+		</ol>
+	);
+}
+
+function ItemTypeCard(props: {
+	itemType: PublicCategoryItemType;
+	items: PublicCategoryItem[];
+}) {
+	const { itemType, items } = props;
+
+	const itemCt = items.length;
+
+	return (
+		<Card>
+			<header>
+				<h2 className="text-2xl font-semibold">{itemType.name}</h2>
+			</header>
+
+			<footer
+				className={cn(
+					"text-sm dark:text-stone-400",
+					"flex items-center justify-between gap-3",
+				)}
+			>
+				<p>
+					Qty: <span className="font-bold">{itemCt}</span>
+				</p>
+			</footer>
+		</Card>
 	);
 }
