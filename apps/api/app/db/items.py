5acad8d8df035e1fc43a2cb114edb96006270e28
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2025-08-23 05:20:37 +0800

    fix(api.db): correct item field types

--- apps/api/app/db/items.py
+++ apps/api/app/db/items.py
@@ -7,7 +7,7 @@ class Item(SQLModel, table=True):
     id: int = Field(primary_key=True)
     uuid: UUID = Field(index=True, unique=True, default_factory=uuid4)
 
-    category_id: int = Field(index=True, unique=True)
-    item_type_id: int = Field(index=True, unique=True)
+    category_id: UUID = Field(index=True)
+    item_type_id: UUID = Field(index=True)
 
     name: str
