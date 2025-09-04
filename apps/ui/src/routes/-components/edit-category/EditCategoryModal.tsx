564b5278398f86de3572164c12de3d9ca97b5fcd
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2025-09-04 22:23:25 +0800

    feat(ui.routes.categories): add edit category modal logic

--- apps/ui/src/routes/-components/edit-category/EditCategoryModal.tsx
+++ apps/ui/src/routes/-components/edit-category/EditCategoryModal.tsx
@@ -1,3 +1,4 @@
+import { type PublicCategory, editCategory } from "@/core/api/codegen";
 import {
 	Dialog,
 	DialogContent,
@@ -5,12 +6,85 @@ import {
 	DialogHeader,
 	DialogTitle,
 } from "@/core/components/ark-ui/Dialog";
+import { cn } from "@/lib/tailwind";
+import { useRouter } from "@tanstack/react-router";
+import { useForm } from "react-hook-form";
 import { useEditCategoryModalStore } from "./useEditCategoryModalStore";
+
+function EditCategoryForm(props: {
+	category: PublicCategory;
+	onSuccess?: () => void;
+}) {
+	const { category } = props;
+
+	const router = useRouter();
+
+	const form = useForm<{
+		categoryName: string;
+	}>({
+		defaultValues: {
+			categoryName: category.name,
+		},
+	});
+
+	const updateCategory = form.handleSubmit(async (formData) => {
+		const result = await editCategory({
+			path: {
+				uuid: category.uuid,
+			},
+			body: {
+				category_name: formData.categoryName,
+			},
+		});
+		if (result.error) {
+			form.setError("root", {
+				message: "Failed updating category",
+			});
+
+			return;
+		}
+
+		await router.invalidate();
+		props.onSuccess?.();
+	});
+
+	return (
+		<form onSubmit={updateCategory}>
+			<fieldset disabled={form.formState.isSubmitting} className="space-y-6">
+				<label className="grid gap-1">
+					<span className="font-semibold">Category Name</span>
+					<input
+						{...form.register("categoryName", { required: true, min: 1 })}
+						className={cn("bg-muted-0", "px-3 py-2")}
+					/>
+				</label>
+
+				<footer className="flex flex-row-reverse">
+					<button
+						type="submit"
+						className={cn(
+							"bg-muted-0-reverse text-muted-0",
+							"enabled:hover:cursor-pointer",
+							"px-3 py-2",
+							"font-semibold",
+						)}
+					>
+						Save
+					</button>
+				</footer>
+			</fieldset>
+		</form>
+	);
+}
 
 export function EditCategoryModal() {
 	const store = useEditCategoryModalStore();
 	const { isOpen, setIsOpen } = store;
 	const { category } = store;
+
+	function closeModal() {
+		setIsOpen(false);
+	}
 
 	if (category === null) {
 		return null;
@@ -27,6 +101,8 @@ export function EditCategoryModal() {
 						Edit a category
 					</DialogDescription>
 				</DialogHeader>
+
+				<EditCategoryForm category={category} onSuccess={closeModal} />
 			</DialogContent>
 		</Dialog>
 	);
