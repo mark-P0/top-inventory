c4aea6a1f2e286aecc6c25544ee1dc4d2248cd3b
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2025-09-04 21:49:39 +0800

    refactor(api.routes.categories): move public category creator to validations

--- apps/api/app/routes/categories/__init__.py
+++ apps/api/app/routes/categories/__init__.py
@@ -2,15 +2,12 @@ from fastapi import APIRouter, HTTPException, status
 
 from app.db import SessionDependency
 from app.db.categories import create_category, get_all_categories
-from app.db.models import Category
 from app.routes.categories.validations import (
     GetCategoriesQuery,
     GetCategoriesResponse,
     NewCategoryBody,
     NewCategoryResponse,
     PublicCategory,
-    PublicCategoryItem,
-    PublicCategoryItemType,
 )
 
 CategoriesRouter = APIRouter(prefix="/categories")
@@ -21,44 +18,30 @@ def get_categories(
     session: SessionDependency,
     query: GetCategoriesQuery,
 ) -> GetCategoriesResponse:
-    def generate_items(category: Category):
-        if not query.include_items:
-            return
+    db_categories = get_all_categories(
+        session,
+        filter={
+            "name_id": query.filter_name_id,
+        },
+    )
 
-        for item in category.items:
-            yield PublicCategoryItem(
-                uuid=str(item.uuid),
-                name=item.name,
-                item_type=PublicCategoryItemType(
-                    uuid=str(item.item_type.uuid),
-                    name=item.item_type.name,
-                ),
-            )
-
-    def generate_categories():
-        categories = get_all_categories(
-            session,
-            filter={
-                "name_id": query.filter_name_id,
-            },
+    public_categories = [
+        PublicCategory.from_db(
+            category,
+            with_items=query.include_items,
         )
-
-        for category in categories:
-            yield PublicCategory(
-                uuid=str(category.uuid),
-                name_id=category.name_id,
-                name=category.name,
-                items=[*generate_items(category)],
-            )
+        for category in db_categories
+    ]
 
     return GetCategoriesResponse(
-        data=[*generate_categories()],
+        data=public_categories,
     )
 
 
 @CategoriesRouter.post("/")
 def new_category(
-    session: SessionDependency, body: NewCategoryBody
+    session: SessionDependency,
+    body: NewCategoryBody,
 ) -> NewCategoryResponse:
     result = create_category(
         session,
@@ -78,10 +61,8 @@ def new_category(
         )
 
     return NewCategoryResponse(
-        data=PublicCategory(
-            uuid=str(category.uuid),
-            name_id=category.name_id,
-            name=category.name,
-            items=[],
+        data=PublicCategory.from_db(
+            category,
+            with_items=False,
         ),
     )
--- apps/api/app/routes/categories/validations.py
+++ apps/api/app/routes/categories/validations.py
@@ -3,6 +3,8 @@ from typing import Annotated
 from fastapi import Query
 from pydantic import BaseModel
 
+from app.db.models import Category
+
 
 class PublicCategoryItemType(BaseModel):
     uuid: str
@@ -16,6 +18,18 @@ class PublicCategoryItem(BaseModel):
     name: str
     item_type: PublicCategoryItemType
 
+    @staticmethod
+    def generate_from_db(category: Category):
+        for item in category.items:
+            yield PublicCategoryItem(
+                uuid=str(item.uuid),
+                name=item.name,
+                item_type=PublicCategoryItemType(
+                    uuid=str(item.item_type.uuid),
+                    name=item.item_type.name,
+                ),
+            )
+
 
 class PublicCategory(BaseModel):
     uuid: str
@@ -23,6 +37,23 @@ class PublicCategory(BaseModel):
 
     name: str
     items: list[PublicCategoryItem]
+
+    @staticmethod
+    def from_db(
+        category: Category,
+        /,
+        with_items: bool = True,
+    ):
+        items = []
+        if with_items:
+            items = [*PublicCategoryItem.generate_from_db(category)]
+
+        return PublicCategory(
+            uuid=str(category.uuid),
+            name_id=category.name_id,
+            name=category.name,
+            items=items,
+        )
 
 
 class GetCategoriesQueryRaw(BaseModel):
