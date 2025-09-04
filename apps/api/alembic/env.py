85207a5f06fc01c6d8c4c6eb995a59c7db142bd2
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2025-09-04 23:06:04 +0800

    chore(api.alembic): assign db url from env

--- apps/api/alembic/env.py
+++ apps/api/alembic/env.py
@@ -1,13 +1,18 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 
 from alembic import context
+from app.core.settings import Settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+"""
+Assign database URL from environment variables
+"""
+config.set_main_option("sqlalchemy.url", Settings.DB_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -64,9 +69,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
